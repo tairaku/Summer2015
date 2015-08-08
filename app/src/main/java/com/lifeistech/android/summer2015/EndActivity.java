962e8521aba3d54cfe6b4f560561f00b3e046231
tsumugi <tsumugi.1218@gmail.com>
2015-08-09 05:16:46 +0900

    7

--- app/src/main/java/com/lifeistech/android/summer2015/EndActivity.java
+++ app/src/main/java/com/lifeistech/android/summer2015/EndActivity.java
@@ -1,30 +1,23 @@
 package com.lifeistech.android.summer2015;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class YossiActivity extends ActionBarActivity {
+public class EndActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_yossi);
-    }
-
-    public void toEnd(View v){
-        Intent intent = new Intent(this, EndActivity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_end);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_yossi, menu);
+        getMenuInflater().inflate(R.menu.menu_end, menu);
         return true;
     }
 
